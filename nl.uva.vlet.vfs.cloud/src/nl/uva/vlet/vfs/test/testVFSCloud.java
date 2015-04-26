7a6ccb577d84b29a46f22fb9fc6fe58766de891f
skoulouzis <skoulouzis@uva.nl>
2015-04-26 16:45:19 +0200

    Added location in test.pop

--- nl.uva.vlet.vfs.cloud/src/nl/uva/vlet/vfs/test/testVFSCloud.java
+++ nl.uva.vlet.vfs.cloud/src/nl/uva/vlet/vfs/test/testVFSCloud.java
@@ -27,6 +27,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import nl.uva.vlet.exception.VRLSyntaxException;
@@ -48,8 +50,9 @@ public class testVFSCloud extends testVFS {
 
     static {
         try {
+            testLoc = new VRL(getCloudProperties().getProperty("test.location"));
 //            testLoc = new VRL("swift://149.156.10.131:8443/auth/v1.0/testBlobStoreVFS");
-            testLoc = new VRL("swift://10.100.0.24:5000/v2.0/testBlobStoreVFS");
+//            testLoc = new VRL("swift://10.100.0.24:5000/v2.0/testBlobStoreVFS");
 //            testLoc = new VRL(
 //                    "swift://10.0.3.208:8080/auth/v1.0/testBlobStoreVFS");
 //             testLoc = new VRL(
@@ -64,7 +67,11 @@ public class testVFSCloud extends testVFS {
 //                    "filesystem:/testBlobStoreVFS");
         } catch (VRLSyntaxException e) {
             // TODO Auto-generated catch block
-            e.printStackTrace();
+            Logger.getLogger(testVFSCloud.class.getName()).log(Level.SEVERE, null, e);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(testVFSCloud.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(testVFSCloud.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
